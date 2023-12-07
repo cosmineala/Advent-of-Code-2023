102076570131817218eea2462ebf62af95b34ecd
Capisizu Cosmin <cosmineala@gmail.com>
2023-12-07 02:31:35 +0200

    style calendar page

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ export default function Home() {
 
 
 
-      <div className='p-5 grid grid-cols-3 lg:grid-cols-7 gap-4 text-xl max-w-fit' >
+      <div className='p-5 grid grid-cols-4 sm:grid-cols-7 gap-4 text-xl max-w-fit' >
 
         {/* empty spaces */}
         {Array.from({ length: 4 }, (_, i) => i + 1).map(i =>
-          <div key={i} className='hidden lg:block' ></div>
+          <div key={i} className='hidden sm:block' ></div>
         )}
 
         {Array.from({ length: 25 }, (_, i) => i + 1).map(day =>
-          <Link key={day} href={`./Day${day}`} className='text-primary hover:text-primaryHover' >[Day {day < 10 && <span>&nbsp;</span>}{day}]</Link>
+          <Link key={day} href={`./Day${day}`} className='text-2xl text-primary hover:text-primaryHover' >[{day < 10 && 0}{day}]</Link>
         )}
 
       </div>
