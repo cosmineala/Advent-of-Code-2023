d188108df6e5e63ea3bcf62c672554c4370649e8
Capisizu Cosmin <cosmineala@gmail.com>
2023-12-07 02:31:10 +0200

    Fix main page keys

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default function Home() {
       <div className='p-5 grid grid-cols-3 lg:grid-cols-7 gap-4 text-xl max-w-fit' >
 
         {/* empty spaces */}
-        {Array.from({ length: 25 }, (_, i) => i + 1).map(day =>
-          <div className='hidden lg:block' ></div>
+        {Array.from({ length: 4 }, (_, i) => i + 1).map(i =>
+          <div key={i} className='hidden lg:block' ></div>
         )}
 
         {Array.from({ length: 25 }, (_, i) => i + 1).map(day =>
-          <Link href={`./Day${day}`} className='text-primary hover:text-primaryHover' >[Day {day < 10 && <span>&nbsp;</span>}{day}]</Link>
+          <Link key={day} href={`./Day${day}`} className='text-primary hover:text-primaryHover' >[Day {day < 10 && <span>&nbsp;</span>}{day}]</Link>
         )}
 
       </div>
